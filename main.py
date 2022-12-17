30b69355e057b7ce617c7c4f1e7e0fa14bae7ba4
amedtamimi <31901003008@std.bau.edu.jo>
2022-12-17 15:51:07 +0300

    solve big moshkelh kanet ra7 tseer m3na

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5be7dc89b1d5d2086551c441867993b33c55e7126ac14d78ae530f96bb89501
-size 1411
+oid sha256:dbf8cc8070a95fc86efc92b94645818a580eb9d94ed6e707a028f8e795ef1d89
+size 1816
