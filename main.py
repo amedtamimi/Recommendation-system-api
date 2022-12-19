4d3484670fe8930fcee75b556fffa82bf8405e6c
amedtamimi <31901003008@std.bau.edu.jo>
2022-12-19 11:39:33 +0300

    changes

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbf8cc8070a95fc86efc92b94645818a580eb9d94ed6e707a028f8e795ef1d89
-size 1816
+oid sha256:44a9d73ed56431acd727f3f899d4461007221a363b6448ad06c9bcf03496060a
+size 1944
